1c0a8a2f2557494231024f9b210d2c85b216d898
Kira Corbin <madkatkeys@gmail.com>
2019-07-09 22:36:02 -0700

    up and running with quit and buy choices

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -11,7 +11,53 @@ const connection = mysql.createConnection({
     database: "bamazonDB",
     port: 3306
 });
+function start() {
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            name: "buyingProduct",
+            // message: "Enter the new Product ID of the item you want to purchase.",
+            choices: ["Enter the new Product ID of the item you want to purchase.", "Exit Bamazon Shopping"],
+            // [Quit with Q]",
+            // validate: function(val) {
+            //   return val > 0 || val.toLowerCase() === "q";
+        },
+    ])
+        .then(function (answers) {
+            if (answers.buyingProduct.indexOf( "Exit") >=0 ){
+                process.exit()
+            }
+            console.log(answers.buyingProduct)
+            if (answers.buyingProduct.indexOf("Enter")  >= 0){
+                buyMoreProduct ()
+            }
+    
+            // console.log(answers)
+            // connection.query("SELECT * FROM products WHERE id = " + answers.buyingProduct, function (err, res) {
+            //     if (err) {
+            //         throw err;
+            
+            //     }
+            //     else {
+            //         if (answers.quantity > res[0].stock_quantity)
+            //         console.log("Insufficent quantity!")
+            //         else {
+            //             console.log("order placed")
+            //         }
+            //             // (res[0].stock_quantity < answers.stock_quantity);
+            //     }
+                
+            // });
+        });
+}
 
+
+// let data = result[0];
+// let stock_quantity = data.stock_quantity;
+// let addNewQuantity = parseInt(stock_quantity) + parseInt(addNewQuantity);
+
+
+function buyMoreProduct () {
 inquirer.prompt([
     {
         type: "input",
@@ -24,67 +70,28 @@ inquirer.prompt([
         message: "How many units of the product they would like to buy?"
     }
 ])
-    .then(function (answers) {
-        console.log(answers)
-        connection.query("SELECT * FROM products WHERE id = " + answers.buyingProduct, function (err, res) {
-            if (err) {
-                throw err;
-        
-            }
-            else {
-                if (answers.quantity > res[0].stock_quantity)
-                console.log("Insufficent quantity!")
-                else {
-                    console.log("order placed")
-                }
-                buyMoreProduct()
-                    // (res[0].stock_quantity < answers.stock_quantity);
-            }
-            
-        });
-    });
-
-// let data = result[0];
-// let stock_quantity = data.stock_quantity;
-// let addNewQuantity = parseInt(stock_quantity) + parseInt(addNewQuantity);
-
-
-function buyMoreProduct () {
-inquirer.prompt([
-    {
-        type: "input",
-        name: "buyingProduct",
-        message: "Enter the new Product ID of the item you want to purchase."
-        // choices: "Exit Bamazon Shopping"
-        // [Quit with Q]",
-        // validate: function(val) {
-        //   return val > 0 || val.toLowerCase() === "q";
-    },
-    {
-        type: "input",
-        name: "quantity",
-        message: "How many units of the new product they would like to buy?"
-    }
-])
    
     .then(function (answers) {
         console.log(answers)
+        console.log("answers.buyingProduct " + answers.buyingProduct)
+        
         connection.query("SELECT * FROM products WHERE id = " + answers.buyingProduct, function (err, res) {
             if (err) {
                 throw err;
         
             }
             else {
+             
                 if (answers.quantity > res[0].stock_quantity)
                 console.log("Insufficent quantity!")
                 else {
                     console.log("order placed")
                 }
-                buyMoreProduct ()
+                start ()
                     // (res[0].stock_quantity < answers.stock_quantity);
             }
             
         });
     });
 }
-
+start()
